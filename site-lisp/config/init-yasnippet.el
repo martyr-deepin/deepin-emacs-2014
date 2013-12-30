769a5d62439263459c6838aab91b2e46e47fe998
Andy Stewart <lazycat.manatee@gmail.com>
2013-12-30 20:21:13 +0800

    make term-mode not enable yasnippet mode.

--- site-lisp/config/init-yasnippet.el
+++ site-lisp/config/init-yasnippet.el
@@ -80,6 +80,12 @@
 (add-to-list `yas/root-directory "/usr/share/deepin-emacs/site-lisp/extensions/yasnippet/snippets/")
 (yas-global-mode 1)
 
+;; Disable yasnippet mode on some mode.
+(dolist (hook (list
+               'term-mode-hook
+               ))
+  (add-hook hook '(lambda () (yas-minor-mode -1))))
+
 (provide 'init-yasnippet)
 
 ;;; init-yasnippet.el ends here
